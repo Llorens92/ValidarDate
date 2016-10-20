6f3910ad44a5546ff696e6c71612b11cb491dede
Llorens92 <tamer_1405@hotmail.com>
2016-10-20 13:32:58 +0200

    Add files via upload

--- public_html/js/Validar.js
+++ public_html/js/Validar.js
@@ -116,61 +116,93 @@ function ValidarNotEmpty() {
             if (form.elements[i].checked) {
                 radio = true;
                 contadorChecks++;
-                }
             }
         }
-        var final = false;
-        if (contadorChecks !== 1 && !emptys)
-            alert("Debe escoger una opcion en el campo Sexo");
-        else if (!emptys)
-            final = true;
-        return final;
     }
-    function ValidarNotEmptyNif() {
-        var correcto = false;
-        var nif = document.getElementsByTagName("input")[0].value;
-        if (nif.length === 0) {
-            alert("No ha rellenado el campo NIF");
-            correcto = true;
+    var final = false;
+    if (contadorChecks !== 1 && !emptys)
+        alert("Debe escoger una opcion en el campo Sexo");
+    else if (!emptys)
+        final = true;
+    return final;
+}
+function ValidarNotEmptyNif() {
+    var correcto = false;
+    var nif = document.getElementsByTagName("input")[0].value;
+    if (nif.length === 0) {
+        alert("No ha rellenado el campo NIF");
+        correcto = true;
+    }
+    return !correcto;
+}
+
+
+function ValidarNoEspacios() {
+    var nums = document.getElementsByTagName("input")[0].value;
+    var array = nums.split(" ");
+    var hayEspacios = false;
+    if (array.length > 1)
+        hayEspacios = true;
+    if (hayEspacios) {
+        alert("Ha introducido espacios en blanco");
+    }
+    return !hayEspacios;
+}
+
+function Validar9Digitos() {
+    var nums = document.getElementsByTagName("input")[0].value;
+    var array = nums.split("");
+    var NueveDig = true;
+    if (array.length !== 9)
+        NueveDig = false;
+    if (!NueveDig) {
+        alert("Debe introducir 9 dígitos");
+    }
+    return NueveDig;
+}
+
+function ValidarDate() {
+    var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth();
+    var yyyy = today.getFullYear();
+    var numDias;
+    if (mm === 1 || mm === 3 || mm === 5 || mm === 7 || mm === 8 || mm === 10 || mm === 12) {
+        numDias=31;
+    } else if (mm === 2) {        
+        numDias=28;
+    } else {        
+        numDias=30;
+    }
+    document.write("<select>");
+    for (var i = 0; i < numDias; i++) {
+        if (dd=== (i+1)) {
+            document.write("<option selected value=" + (i+1) + ">" + (i+1) + "</option>");
+        } else {
+            document.write("<option value=" + (i+1) + ">" + (i+1) + "</option>");
         }
-        return !correcto;
     }
-
-
-    function ValidarNoEspacios() {
-        var nums = document.getElementsByTagName("input")[0].value;
-        var array = nums.split(" ");
-        var hayEspacios = false;
-        if (array.length > 1)
-                hayEspacios = true;
-        if (hayEspacios) {
-            alert("Ha introducido espacios en blanco");
+    document.write("</select>");
+    mm = meses[mm];
+    document.write("<select>");
+    for (var i = 0; i < 12; i++) {
+        if (mm === meses[i]) {
+            document.write("<option selected value=" + meses[i] + ">" + meses[i] + "</option>");
+        } else {
+            document.write("<option value=" + meses[i] + ">" + meses[i] + "</option>");
         }
-        return !hayEspacios;
     }
-
-    function Validar9Digitos() {
-        var nums = document.getElementsByTagName("input")[0].value;
-        var array = nums.split("");
-        var NueveDig = true;
-        if (array.length !== 9)
-            NueveDig = false;
-        if (!NueveDig) {
-            alert("Debe introducir 9 dígitos");
+    document.write("</select>");
+    document.write("<select>");
+    for (var i = 1916; i < 2036; i++) {
+        if (yyyy === i) {
+            document.write("<option selected value=" + i + ">" + i + "</option>");
+        } else {
+            document.write("<option value=" + i + ">" + i + "</option>");
         }
-        return NueveDig;
     }
-
-    function ValidarDate() {
-        var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth();
-        var yyyy = today.getFullYear();
-        mm = meses[mm];
-        for (var i = 0; i < 12; i++){
-        document.write("<select><option value=>"+"</select>");
-    }
+    document.write("</select>");
 }
 
 
